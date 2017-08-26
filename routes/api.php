698f97c8636bd02a4f343321d2f337c7bc6a6cfd
Dallin <dallinis@hotmail.com>
2017-08-25 19:46:50 -0700

    adds ability to create grocery list item on the backend

--- routes/api.php
+++ routes/api.php
@@ -21,6 +21,7 @@ Route::group([
     Route::post('grocery-list/{grocerylist}/delete', 'GroceryListController@delete');
     Route::patch('grocery-list/{grocerylist}', 'GroceryListController@update');
 
+    Route::post('grocery-list-item', 'GroceryListItemController@store');
     Route::post('grocery-list-item-completion/{groceryListItem}', 'GroceryListItemCompletionController@store');
 
     // vue-router will handle any other requests
