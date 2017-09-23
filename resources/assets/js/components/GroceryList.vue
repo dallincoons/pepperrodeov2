4e7457d9e59b2ad71684a8041e24b60e7a679e89
Dallin <dallinis@hotmail.com>
2017-09-22 19:54:12 -0700

    broke out edit item modal to it's own component, cleaned up code

--- resources/assets/js/components/GroceryList.vue
+++ resources/assets/js/components/GroceryList.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container">
 
-        <div class="container-heading"><h2>{{listTitle}}</h2></div>
+        <div class="container-heading"><h2>{{list.title}}</h2></div>
 
         <div class="container-body">
             <div class="add-item-wrapper">
@@ -21,58 +21,13 @@
                         <span @click="toggleItem(item.id)" class="checkbox" v-bind:class="{checkmark : item.is_checked}"></span>
                         <span class="item" v-bind:class="{checked : item.is_checked}">{{item.quantity}} {{item.description}}</span>
 
-                        <modal v-if="showModal" @close="displayModal">
-
-                            <span class="close-modal" @click="displayModal" transition="modal">x</span>
-                            <h3 class="modal-header">Edit Item</h3>
-                            <div class="modal-edit-section">
-                                <input class="modal-edit-input" :value="itemToUpdate.description" v-model="itemToUpdate.description">
-                                <button class="modal-button" @click="updateItem(item)">Save</button>
-
-                            </div>
-                            <div class="modal-edit-footer">
-               <span class="modal-delete-img"> <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 59 59" style="enable-background:new 0 0 59 59;" xml:space="preserve" width="16px" height="16px">
-<g>
-	<path d="M29.5,51c0.552,0,1-0.447,1-1V17c0-0.553-0.448-1-1-1s-1,0.447-1,1v33C28.5,50.553,28.948,51,29.5,51z" fill="#ff4b2e"/>
-	<path d="M19.5,51c0.552,0,1-0.447,1-1V17c0-0.553-0.448-1-1-1s-1,0.447-1,1v33C18.5,50.553,18.948,51,19.5,51z" fill="#ff4b2e"/>
-	<path d="M39.5,51c0.552,0,1-0.447,1-1V17c0-0.553-0.448-1-1-1s-1,0.447-1,1v33C38.5,50.553,38.948,51,39.5,51z" fill="#ff4b2e"/>
-	<path d="M52.5,6H38.456c-0.11-1.25-0.495-3.358-1.813-4.711C35.809,0.434,34.751,0,33.499,0H23.5c-1.252,0-2.31,0.434-3.144,1.289   C19.038,2.642,18.653,4.75,18.543,6H6.5c-0.552,0-1,0.447-1,1s0.448,1,1,1h2.041l1.915,46.021C10.493,55.743,11.565,59,15.364,59   h28.272c3.799,0,4.871-3.257,4.907-4.958L50.459,8H52.5c0.552,0,1-0.447,1-1S53.052,6,52.5,6z M21.792,2.681   C22.24,2.223,22.799,2,23.5,2h9.999c0.701,0,1.26,0.223,1.708,0.681c0.805,0.823,1.128,2.271,1.24,3.319H20.553   C20.665,4.952,20.988,3.504,21.792,2.681z M46.544,53.979C46.538,54.288,46.4,57,43.636,57H15.364   c-2.734,0-2.898-2.717-2.909-3.042L10.542,8h37.915L46.544,53.979z" fill="#ff4b2e"/>
-</g>
-<g>
-</g>
-<g>
-</g>
-<g>
-</g>
-<g>
-</g>
-<g>
-</g>
-<g>
-</g>
-<g>
-</g>
-<g>
-</g>
-<g>
-</g>
-<g>
-</g>
-<g>
-</g>
-<g>
-</g>
-<g>
-</g>
-<g>
-</g>
-<g>
-</g>
-</svg></span><span class="modal-delete" @click="deleteItem(item)">Delete Item</span>
-                            </div>
-                            <div class="modal-line"></div>
-                        </modal>
-
+                        <edit-item-modal
+                            v-if="showModal"
+                            :itemToUpdate="itemToUpdate"
+                            @close="hideModal"
+                            @update="updateItem"
+                            @delete="deleteItem"
+                        ></edit-item-modal>
                     </li>
 
                 </ul>
@@ -84,20 +39,20 @@
 </template>
 
 <script>
-    import Modal from './Modal.vue';
+    import EditItemModal from './GroceryList/EditItemModal.vue';
 
     export default {
+        
         components : {
-            'modal' : Modal
+            EditItemModal
         },
+
         data(){
             return {
                 showModal   : false,
+                list        : {},
                 listId      : '',
-                listTitle   : '',
-                listitems   : '',
                 description : '',
-                editingItem : '',
                 departments : '',
                 department  : '',
                 itemToUpdate : ''
@@ -106,7 +61,7 @@
 
         computed : {
             itemsGrouped : function () {
-                return _.chain(this.listitems)
+                return _.chain(this.list.items)
                     .sortBy(function (item) {
                         return item.department.name;
                     })
@@ -117,39 +72,35 @@
         },
 
         mounted() {
-
             this.listId = this.$route.params.id;
-            axios.get('/api/v1/grocery-list/' + this.listId).then((response) => {
-                let responseData = response.data;
-                this.listTitle = responseData.title;
-                this.listitems = responseData.items;
-
-            });
-
-
-            axios.get('/api/v1/departments').then((response) => {
-                this.departments = response.data;
-            });
-
-
+            this.getList();
+            this.getDepartments();
         },
         methods    : {
 
-            displayModal() {
+            hideModal() {
                 this.showModal = false;
+            },
+
+            displayModal() {
+                this.showModal = true;
             },
 
             getList() {
                 axios.get('/api/v1/grocery-list/' + this.listId).then((response) => {
-                    let responseData = response.data;
-                    this.listTitle = responseData.title;
-                    this.listitems = responseData.items;
+                    this.list = response.data;
+                });
+            },
+
+            getDepartments() {
+                axios.get('/api/v1/departments').then((response) => {
+                    this.departments = response.data;
                 });
             },
 
             toggleItem(itemId) {
                 axios.post('/api/v1/grocery-list-item-completion/' + itemId).then((response) => {
-                    let itemToUpdate        = this.listitems.find(function (item) {
+                    let itemToUpdate = this.list.items.find(function (item) {
                         return item.id == itemId;
                     });
                     itemToUpdate.is_checked = response.data.is_checked;
@@ -164,39 +115,33 @@
                 };
                 axios.post('/api/v1/grocery-list-item', itemDetails).then((response) => {
                     this.getList();
-                    this.description = '';
-                    this.department  = '';
+                    this.clearForm();
                 })
             },
 
-            editItem(item) {
-                EventBus.$emit('grocerylist.update.show', {item : item});
+            clearForm() {
+                this.description = '';
+                this.department  = '';
             },
 
-            updateItem() {
-                this.showModal = false;
-                axios.patch('/api/v1/grocery-list-item/' + this.itemToUpdate.id, {description : this.itemToUpdate.description}).then((response) => {
+            updateItem(itemToUpdate) {
+                this.hideModal();
+                axios.patch('/api/v1/grocery-list-item/' + itemToUpdate.id, {description : itemToUpdate.description}).then((response) => {
                     this.getList();
                 });
-
-
             },
 
             openEditItem(item) {
-                this.showModal = true;
                 this.itemToUpdate = item;
-
-
+                this.displayModal();
             },
 
-            deleteItem() {
-                this.showModal = false;
-                axios.delete('/api/v1/grocery-list-item/' + this.itemToUpdate.id).then((response) => {
+            deleteItem(itemToUpdate) {
+                this.hideModal();
+                axios.delete('/api/v1/grocery-list-item/' + itemToUpdate.id).then((response) => {
                     this.getList();
                 });
             }
-
-
         }
     }
 </script>
@@ -210,6 +155,7 @@
         justify-content: center;
         align-items: center;
     }
+
     .container {
         width: 60%;
         background: #fff;
@@ -221,7 +167,6 @@
         box-shadow: 0 0 15px 5px rgba(27,26,26,0.14);
         margin-bottom: 40px;
         margin-top: 50px;
-
     }
 
     .container-heading {
@@ -330,6 +275,7 @@
         margin: 0;
     }
 
+    /* displayed dynamically */
     .checkmark {
         background: rgba(25, 25, 25, .3);
     }
@@ -337,84 +283,5 @@
     .item {
         padding-left: 10px;
     }
-
-    .modal-line {
-        height: 5px;
-        background-color: #ff4b2e;
-        border-radius: 0 0 5px 5px;
-        position: absolute;
-        bottom: 0;
-        width: 100%;
-
-    }
-
-    .modal-header {
-        margin-top: 0;
-        color: #4f4f4f;
-        text-align: center;
-        font-weight: 300;
-    }
-
-    .modal-edit-section {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        margin: 15px;
-    }
-
-    .modal-button {
-        background: #ff4b2e;
-        color: #FFFFFF;
-        margin: 15px 0;
-        border: 0;
-        border-radius: 5px;
-        padding: 5px 20px;
-        font-weight: 700;
-        font-size: 1.2em;
-    }
-
-    .modal-button:hover {
-        background: #BF3822;
-        transition: background 2s;
-    }
-
-    .modal-edit-input {
-        width: 100%;
-        background: #ebebeb;
-        border: none;
-        font-size: 1.2em;
-        padding: 5px 20px;
-    }
-
-    .modal-edit-footer {
-        align-self: flex-end;
-        text-align: right;
-        color: #ff4b2e;
-        padding: 2px;
-        margin-left: 15px;
-    }
-
-    .modal-edit-footer:hover {
-        cursor: pointer;
-    }
-
-    .modal-delete {
-        margin: 0 15px 0 2px;
-    }
-    .close-modal {
-        position: absolute;
-        right: 0;
-        top: 0;
-        font-weight: 700;
-        font-size: 1.2em;
-        margin-right: 5px;
-    }
-
-    .close-modal:hover {
-        cursor: pointer;
-    }
-
-
-
 
 </style>
---
+++ resources/assets/js/components/GroceryList/EditItemModal.vue
@@ -0,0 +1,128 @@
+<template>
+    <modal @close="displayModal">
+
+        <span class="close-modal" @click="displayModal" transition="modal">x</span>
+        <h3 class="modal-header">Edit Item</h3>
+        <div class="modal-edit-section">
+            <input class="modal-edit-input" :value="itemToUpdate.description" v-model="itemToUpdate.description">
+            <button class="modal-button" @click="updateItem">Save</button>
+        </div>
+        <div class="modal-edit-footer">
+            <trash-can></trash-can>
+            <span class="modal-delete" @click="deleteItem">Delete Item</span>
+        </div>
+        <div class="modal-line"></div>
+    </modal>
+</template>
+
+<script>
+    import Modal from '../Modal.vue';
+    import TrashCan from '../assets/trashcan.vue';
+
+    export default {
+        props : ['itemToUpdate'],
+        components : {
+            'modal' : Modal,
+            TrashCan
+        },
+        mounted() {
+
+        },
+        data() {
+            return {
+
+            }
+        },
+        methods : {
+            displayModal() {
+                this.$emit('close');
+            },
+
+            updateItem() {
+                this.$emit('update', this.itemToUpdate);
+            },
+
+            deleteItem() {
+                this.$emit('delete', this.itemToUpdate);
+            }
+        }
+    }
+</script>
+
+<style>
+    .modal-line {
+        height: 5px;
+        background-color: #ff4b2e;
+        border-radius: 0 0 5px 5px;
+        position: absolute;
+        bottom: 0;
+        width: 100%;
+
+    }
+
+    .modal-header {
+        margin-top: 0;
+        color: #4f4f4f;
+        text-align: center;
+        font-weight: 300;
+    }
+
+    .modal-edit-section {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        margin: 15px;
+    }
+
+    .modal-button {
+        background: #ff4b2e;
+        color: #FFFFFF;
+        margin: 15px 0;
+        border: 0;
+        border-radius: 5px;
+        padding: 5px 20px;
+        font-weight: 700;
+        font-size: 1.2em;
+    }
+
+    .modal-button:hover {
+        background: #BF3822;
+        transition: background 2s;
+    }
+
+    .modal-edit-input {
+        width: 100%;
+        background: #ebebeb;
+        border: none;
+        font-size: 1.2em;
+        padding: 5px 20px;
+    }
+
+    .modal-edit-footer {
+        align-self: flex-end;
+        text-align: right;
+        color: #ff4b2e;
+        padding: 2px;
+        margin-left: 15px;
+    }
+
+    .modal-edit-footer:hover {
+        cursor: pointer;
+    }
+
+    .modal-delete {
+        margin: 0 15px 0 2px;
+    }
+    .close-modal {
+        position: absolute;
+        right: 0;
+        top: 0;
+        font-weight: 700;
+        font-size: 1.2em;
+        margin-right: 5px;
+    }
+
+    .close-modal:hover {
+        cursor: pointer;
+    }
+</style>
