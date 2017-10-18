fda3af74167cbbe90460ec8c4626cc9c3a4c0692
Dallin <dallinis@hotmail.com>
2017-10-17 19:38:00 -0700

    adds recipe ingredient endpoint

--- app/Repositories/RecipeRepositoryEloquent.php
+++ app/Repositories/RecipeRepositoryEloquent.php
@@ -4,10 +4,20 @@ namespace App\Repositories;
 
 use App\Entities\Ingredient;
 use App\Entities\Recipe;
+use Illuminate\Foundation\Application;
 use Prettus\Repository\Eloquent\BaseRepository;
 
 class RecipeRepositoryEloquent extends BaseRepository implements RecipeRepository
 {
+    private $ingredientRepository;
+
+    public function __construct(Application $app, IngredientRepositoryEloquent $repository)
+    {
+        parent::__construct($app);
+
+        $this->ingredientRepository = $repository;
+    }
+
     /**
      * @param array $attributes
      * @return Recipe
@@ -19,12 +29,17 @@ class RecipeRepositoryEloquent extends BaseRepository implements RecipeRepositor
         $ingredients = array_get($attributes, 'ingredients') ?: [];
 
         foreach($ingredients as $ingredient){
-            Ingredient::create([
+            $this->ingredientRepository->create([
                     'recipe_id' => $recipe->getKey()
                 ] + $ingredient);
         }
 
         return $recipe;
+    }
+
+    public function addIngredient(array $ingredients)
+    {
+        $this->ingredientRepository->create($ingredients);
     }
 
     /**
