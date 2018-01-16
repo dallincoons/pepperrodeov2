3681c66c284f7cf9b4a59ba3566cc9eebde64d5b
Dallin <dallinis@hotmail.com>
2018-01-16 15:28:44 -0700

    adds ability to add department id to listable ingredient as part of an update

--- tests/Feature/Recipe/UpdateRecipeTest.php
+++ tests/Feature/Recipe/UpdateRecipeTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature\Recipe;
 
 use App\Category;
+use App\Entities\Department;
 use App\Entities\Recipe;
 use Tests\Fakers\RecipeFaker;
 use Tests\TestCase;
@@ -125,6 +126,33 @@ class UpdateRecipeTest extends TestCase
 
         $response->assertStatus(200);
         $this->assertCount(2, $recipe->fresh()->listableIngredients);
+    }
+
+    /** @test */
+    public function add_departmentalized_listable_ingredient_to_recipe_as_part_of_update()
+    {
+        $department = factory(Department::class)->create();
+
+        $recipe = RecipeFaker::withItems();
+        $ingredient = $recipe->listableIngredients->first();
+
+        $response = $this->patch($this->api('recipes/' . $recipe->getKey()), [
+            'listable_ingredients' => [
+                [
+                    'id' => $ingredient->getKey(),
+                    'description' => $ingredient->description . 'altered',
+                    'quantity' => null,
+                    'department_id' => $department->getKey()
+                ],
+                [
+                    'description' => 'beat on the brat',
+                    'quantity' => 101,
+                ],
+            ]
+        ]);
+
+        $response->assertStatus(200);
+        $this->assertEquals($department->getKey, array_get(Recipe::first()->listableIngredients->first()->toArray(), 'deparment_id'));
     }
 
     /** @test */
