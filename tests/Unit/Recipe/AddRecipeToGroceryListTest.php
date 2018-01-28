70dac7e265035b982c834e83af1f5e9d09220c8b
Dallin <dallinis@hotmail.com>
2018-01-27 18:16:06 -0700

    adds test for adding duplicate recipes

--- tests/Unit/Recipe/AddRecipeToGroceryListTest.php
+++ tests/Unit/Recipe/AddRecipeToGroceryListTest.php
@@ -28,4 +28,22 @@ class AddRecipeToGroceryListTest extends TestCase
 
         $this->assertCount(count($recipe->listableIngredients), $grocerylist->fresh()->items);
     }
+
+    /** @test */
+    public function it_adds_the_same_recipe_to_list_again()
+    {
+        /** @var $grocerylist GroceryList */
+        $grocerylist = factory(GroceryList::class)->create();
+
+        $recipe = factory(Recipe::class)->create();
+        factory(ListableIngredient::class)->create(['recipe_id' => $recipe]);
+        factory(ListableIngredient::class)->create(['recipe_id' => $recipe]);
+
+        $this->assertCount(0, $grocerylist->items);
+
+        $grocerylist->addRecipe($recipe);
+        $grocerylist->addRecipe($recipe);
+
+        $this->assertCount(count($recipe->listableIngredients) * 2, $grocerylist->fresh()->items);
+    }
 }
