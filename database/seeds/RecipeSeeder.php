e2a3803d86405236f9092178ddcd45bd3c133887
Dallin <dallinis@hotmail.com>
2018-02-04 16:58:26 -0700

    adds additonal api specification for add-recipe endpoint

--- database/seeds/RecipeSeeder.php
+++ database/seeds/RecipeSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Entities\Ingredient;
+use App\Entities\ListableIngredient;
 use App\Entities\Recipe;
 use Illuminate\Database\Seeder;
 
@@ -8,7 +10,9 @@ class RecipeSeeder extends Seeder
     public function run()
     {
         foreach(range(1, 5) as $i) {
-            factory(Recipe::class)->create();
+            $recipe = factory(Recipe::class)->create();
+            $recipe->listableIngredients()->save(factory(ListableIngredient::class)->create());
+            $recipe->ingredients()->save(factory(Ingredient::class)->create());
         }
     }
 }
