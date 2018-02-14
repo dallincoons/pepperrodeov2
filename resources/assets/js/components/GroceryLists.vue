07fcc2981d2489176a4696b8116e29ff2bc49b7a
Dallin <dallinis@hotmail.com>
2018-02-14 12:17:41 -0700

    grocery lists styling updates

--- resources/assets/js/components/GroceryLists.vue
+++ resources/assets/js/components/GroceryLists.vue
@@ -1,17 +1,33 @@
 <template>
     <div class="container">
-        <div class="container-heading">
-        <h2>Lists</h2>
-        <div class="button-wrapper">
-            <router-link class="sq-button" to="/grocery-lists/create"><add-icon-dark class="button-icon"></add-icon-dark> New List</router-link>
-            <button class="sq-button"><search class="button-icon"></search>Search</button>
+        <div class="mod-container" id="newListModal">
+            <div>Modal Thing</div>
         </div>
+
+        <div class="container-heading">
+            <h2 class="small-screen">Lists</h2>
+            <div class="button-wrapper">
+                <button class="sq-button" @click="newListModal"><add-icon-dark class="button-icon"></add-icon-dark> New List</button>
+                <button class="sq-button" @click="toggleSearch"><search class="button-icon"></search>Search</button>
+            </div>
+
+            <div class="drop-wrapper" id="searchBox">
+                <caret class="drop-caret"></caret>
+                <input type="text" class="drop-input"><button class="drop-button">Search</button>
+                <div class="radio-wrapper">
+                    <span><input type="radio" value="Alphabetical"> Alphabetical</span>
+                    <span><input type="radio" value="Recently Added"> Recently Added</span>
+                </div>
+            </div>
+
+
+
         </div>
 
 
         <div class="container-body">
 
-            <ul class="list-container">
+            <ul class="list-container" id="list-container">
                 <li v-for="list in grocerylists" class="grocery-list grow"><router-link :to="{ name: 'grocery-lists', params: { id: list.id }}" >{{list.title}}</router-link></li>
             </ul>
             <add-icon-dark></add-icon-dark>
@@ -24,18 +40,24 @@
     import GroceryLists from './resources/GroceryLists';
     import AddIconDark from './assets/add-icon-dark.vue';
     import Search from './assets/search.vue';
+    import Caret from './assets/caret.vue';
+    import EditItemModal from './GroceryList/EditItemModal.vue';
+
 
     export default {
         components : {
             AddIconDark,
-            Search
+            Search,
+            Caret,
+            EditItemModal,
 
         },
         data(){
             return {
                 grocerylists : [],
                 searchQuery : '',
-                searchClosed : true,
+                searchOpen : false,
+                showModal   : false
             }
         },
 
@@ -51,14 +73,32 @@
                     this.grocerylists = response.data;
                });
             },
-
             toggleSearch() {
-                if(this.searchClosed === true) {
-                    this.searchClosed = false;
+                let listContainer = document.getElementById("list-container");
+                let searchBox = document.getElementById("searchBox");
+                if(this.searchOpen === false) {
+                    this.searchOpen = true;
+                    listContainer.classList.add("margin-transition");
+                    searchBox.classList.add("show-box");
                 } else {
-                    this.searchGroceryLists();
+                    this.searchOpen = false;
+                    listContainer.classList.remove("margin-transition");
+                    searchBox.classList.remove("show-box");
                 }
+
+            },
+            newListModal() {
+                let newListModal = document.getElementById("newListModal");
+                newListModal.classList.add("show-modal");
             }
+
+            // toggleSearch() {
+            //     if(this.searchClosed === true) {
+            //         this.searchClosed = false;
+            //     } else {
+            //         this.searchGroceryLists();
+            //     }
+            // }
         }
     }
 </script>
