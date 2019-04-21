a005c499f63a0b59a2bab805789a854ee38430d4
Dallin <dallinis@hotmail.com>
2019-04-21 09:20:14 -0600

    tweak migration

--- database/migrations/2019_04_20_162204_add_recipe_cascade_to_item_group.php
+++ database/migrations/2019_04_20_162204_add_recipe_cascade_to_item_group.php
@@ -13,21 +13,13 @@ class AddRecipeCascadeToItemGroup extends Migration
      */
     public function up()
     {
-        Schema::table('grocery_list_item_groups', function (Blueprint $table)
-        {
-            $table->dropForeign('grocery_list_item_groups_recipe_id_foreign');
-//            $table->unsignedInteger('recipe_id')->nullable()->change();
+        Schema::table('grocery_list_item_groups', function (Blueprint $table) {
+            \DB::raw('alter table `grocery_list_item_groups` drop key `grocery_list_item_groups_recipe_id_foreign`');
+        });
+
+        Schema::table('grocery_list_item_groups', function (Blueprint $table) {
+            $table->unsignedInteger('recipe_id')->nullable()->change();
             $table->foreign('recipe_id')->references('id')->on('recipes')->onDelete('set null');
         });
-    }
-
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        //
     }
 }
