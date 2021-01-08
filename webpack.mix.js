eb33ab85117152c76d3e6aa3b6588e5d82ed4cb3
Dallin <dallinis@hotmail.com>
2021-01-07 20:36:17 -0700

    add resource file versioning

--- webpack.mix.js
+++ webpack.mix.js
@@ -16,6 +16,7 @@ mix.less('resources/assets/less/app.less', 'public/css')
    .copy('node_modules/sweetalert/dist/sweetalert.min.js', 'public/js/sweetalert.min.js')
    .copy('node_modules/sweetalert/dist/sweetalert.css', 'public/css/sweetalert.css')
     .js('resources/assets/js/app.js', 'public/js')
+    .sourceMaps()
     .vue()
     .copy('resources/assets/img', 'public/img', false)
     .browserSync({
@@ -33,4 +34,5 @@ mix.less('resources/assets/less/app.less', 'public/css')
                 'vue$': 'vue/dist/vue.js'
             }
         }
-   }).sourceMaps();
+   })
+   .version();
