863613e3ec9e424c83166c096649f54a66e6d930
Dallin <dallinis@hotmail.com>
2021-03-12 19:49:21 -0700

    WIP

--- resources/assets/js/components/MealPlanning.vue
+++ resources/assets/js/components/MealPlanning.vue
@@ -27,7 +27,7 @@
             </div>
             <div class="meal-planning-wrapper" v-if="datesSet">
                 <div class="dates-wrapper">
-                    <div class="date-wrapper" v-for="(recipes, date) in scheduledRecipes">
+                    <div class="date-wrapper" v-for="(entries, date) in groupRecipesAndItems">
                         <div class="date-heading">
                             <p>{{prettyDate(date)}}</p>
                         </div>
@@ -38,32 +38,46 @@
                         @dragover.prevent
                         @dragenter.prevent
                         >
-                            <div v-for="recipe in recipes" class="recipe-on-date" :id="date" draggable="true" @dragstart='startDrag($event, recipe)'>
+                            <div v-for="entry in entries" class="recipe-on-date" :id="date" draggable="true" @dragstart='startDrag($event, entry)' data-on-list="recipe">
                                 <div class="recipe-on-date-info" v-if="!editing"  :id="date">
-                                    <span class="date-category" :id="date">{{recipe.category.title}}</span>
-                                    <p @drop='onDrop($event)' :id="date" class="date-recipe-title">{{recipe.title}}</p>
+                                    <span class="date-category" :id="date">{{entry.category}}</span>
+                                    <p @drop='onDrop($event)' :id="date" class="date-recipe-title">{{entry.title}}</p>
                                 </div>
                                 <div class="recipe-on-date-info" v-if="editing">
-                                    <span class="date-category">{{recipe.category.title}}</span>
-                                    <p class="date-recipe-title">{{recipe.title}}</p>
+                                    <span class="date-category">{{entry.category}}</span>
+                                    <p class="date-recipe-title">{{entry.title}}</p>
                                 </div>
-                                <span @click="removeRecipe(date, recipe.id)" class="remove-date-recipe">x</span>
+                                <!--<span @click="removeRecipe(date, recipe.id)" class="remove-date-recipe">x</span>-->
                             </div>
                         </div>
                     </div>
                 </div>
                 <div class="planning-recipes-wrapper">
+                    <div class="section-for-recipes">
+                        <p class="planning-dates">Add an item</p>
+                        <form class="add-item-form">
+                            <input type="text" v-model="itemToAdd" class="add-item-input recipe-input search-box">
+                            <button class="add-item-button" @click="addItem"><add-plus></add-plus></button>
+                        </form>
+                        <ul class="items-added recipes-list">
+                            <li v-for="item in itemsAdded" @dragstart='startItemDrag($event, item)' class="recipe-ingredient item-added" draggable="true" data-on-list="item">{{item}}</li>
+                        </ul>
+                    </div>
                     <div class="search-wrapper meal-planning-search-wrapper">
-                        <input
-                                type="search"
-                                name="recipeSearch"
-                                placeholder="Search"
-                                v-model="itemSearchedFor"
-                                v-on:keyup.enter="searchRecipes(itemSearchedFor)"
-                                class="recipe-input search-box"
-                        >
-                        <button class="close-icon" type="reset" @click="clearSearch()"><x-icon style="width: 15px; height: 15px" class="search-x-icon" :class="{closeIconVisible : itemSearchedFor.length > 0}"></x-icon></button>
-                        <button @click="searchRecipes(itemSearchedFor)" class="search-button"><search class="search-icon"></search></button>
+                        <p class="planning-dates">Add a recipe</p>
+                        <div class="meal-planning-search-input-wrapper">
+                            <input
+                                    type="search"
+                                    name="recipeSearch"
+                                    placeholder="Search"
+                                    v-model="itemSearchedFor"
+                                    v-on:keyup.enter="searchRecipes(itemSearchedFor)"
+                                    class="recipe-input search-box"
+                            >
+                            <button class="close-icon" type="reset" @click="clearSearch()"><x-icon style="width: 15px; height: 15px" class="search-x-icon" :class="{closeIconVisible : itemSearchedFor.length > 0}"></x-icon></button>
+                            <button @click="searchRecipes(itemSearchedFor)" class="search-button"><search class="search-icon"></search></button>
+                        </div>
+
                     </div>
                     <div class="recipes-body">
                         <div class="container-body recipes-wrapper">
@@ -88,11 +102,13 @@
     import moment from 'moment';
     import XIcon from './assets/x-icon';
     import Search from './assets/search.vue';
+    import AddPlus from './assets/add-plus'
 
     export default {
         components : {
             XIcon,
-            Search
+            Search,
+            AddPlus
         },
         props : {
             editing: Boolean
@@ -108,7 +124,10 @@
                 endMax: moment().add(31, 'd').format("YYYY-MM-DD"),
                 datesSet: false,
                 itemSearchedFor: '',
-                draggedFrom: ''
+                draggedFrom: '',
+                itemToAdd: '',
+                itemsAdded: [],
+                scheduledItems: {}
             }
         },
         computed : {
@@ -119,6 +138,48 @@
             },
             endMin() {
                 return this.dateStart;
+            },
+            groupRecipesAndItems() {
+                const thingsOnDay = {};
+                const days = Object.keys(this.scheduledRecipes);
+                for (const day of days) {
+                    thingsOnDay[day] = [];
+                }
+                let startDay = moment(this.dateStart);
+                let amountOfDays = moment(this.dateEnd).diff(this.dateStart, 'days');
+                this.$set(thingsOnDay, startDay.format("YYYY-MM-DD"), []);
+                for (let i = 0; i < amountOfDays; i++) {
+                    this.$set(thingsOnDay, startDay.add(1, 'd').format("YYYY-MM-DD"), []);
+                }
+                // for (const [key, value] of Object.entries(this.scheduledItems)) {
+                //     console.log(`${key}: ${value}`);
+                //     console.log(thingsOnDay[key]);
+                // for (const item in this.scheduledItems) {
+                    // console.log(`${item}: ${this.scheduledItems[item]}`);
+                    // console.log(thingsOnDay[item]);
+                    // thingsOnDay[item].push({
+                    //     title: this.scheduledItems[item],
+                    //     category: 'item',
+                    //     type: 'item'
+                    // });
+                // }
+                // this.scheduledItems.forEach((item) => {
+                //     thingsOnDay.push({
+                //         title: item,
+                //         category: 'item',
+                //         type: 'item'
+                //     });
+                // });
+
+                // this.scheduledRecipes.forEach((recipe) => {
+                //     thingsOnDay.push({
+                //         title: recipe.title,
+                //         category: recipe.category.title,
+                //         type: 'recipe'
+                //     });
+                // });
+
+                return thingsOnDay;
             }
         },
 
@@ -140,16 +201,29 @@
                 if(evt.srcElement.id !== '') {
                     this.draggedFrom = evt.srcElement.id;
                 }
+                if(recipe === '') {
+                    return
+                }
 
                 evt.dataTransfer.dropEffect = 'move';
                 evt.dataTransfer.effectAllowed = 'move';
                 evt.dataTransfer.setData('recipeID', recipe.id);
 
             },
-
             onDrop (evt) {
+                if(evt.dataTransfer.getData('recipeID') === '') {
+                    const item = evt.dataTransfer.getData('item');
+                    let targetID = evt.target.id;
+                    evt.stopPropagation();
+                    this.scheduledItems[targetID].push(item);
+                    return;
+                }
                 const recipeID = evt.dataTransfer.getData('recipeID');
+                if(recipeID === '') {
+                    return
+                }
                 let targetID = evt.target.id;
+                evt.stopPropagation();
                 const recipe = this.recipes.find(recipe => recipe.id == recipeID);
                 this.scheduledRecipes[targetID].push(recipe);
                 if(this.draggedFrom !== '') {
@@ -157,13 +231,28 @@
                     this.draggedFrom = '';
                 }
             },
+            startItemDrag(evt, item) {
+                if(evt.srcElement.id !== '') {
+                    this.draggedFrom = evt.srcElement.id;
+                }
+                if(item === '') {
+                    return
+                }
+                evt.dataTransfer.dropEffect = 'move';
+                evt.dataTransfer.effectAllowed = 'move';
+                evt.dataTransfer.setData('item', item);
+
+            },
 
             setDates() {
                 let amountOfDays = moment(this.dateEnd).diff(this.dateStart, 'days');
                 let startDay = moment(this.dateStart);
                 this.$set(this.scheduledRecipes, startDay.format("YYYY-MM-DD"), []);
+                this.$set(this.scheduledItems, startDay.format("YYYY-MM-DD"), []);
                 for (let i = 0; i < amountOfDays; i++) {
-                    this.$set(this.scheduledRecipes, startDay.add(1, 'd').format("YYYY-MM-DD"), []);
+                    let nextDay = startDay.add(1, 'd');
+                    this.$set(this.scheduledRecipes, nextDay.format("YYYY-MM-DD"), []);
+                    this.$set(this.scheduledItems, nextDay.format("YYYY-MM-DD"), []);
                 }
                 this.datesSet = true;
             },
@@ -211,6 +300,10 @@
                     }
                 }
                 this.scheduledRecipes = result;
+            },
+            addItem() {
+                this.itemsAdded.push(this.itemToAdd);
+                this.itemToAdd = '';
             }
         }
     }
