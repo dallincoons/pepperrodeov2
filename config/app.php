bf37664030768e9324f350049035d8a2d36b2b36
Dallin Coons <dallincoons@Dallins-MacBook-Pro.local>
2023-01-16 21:20:14 -0600

    remove unused packages

--- config/app.php
+++ config/app.php
@@ -166,7 +166,6 @@ return [
         /*
          * Package Service Providers...
          */
-        Laravel\Tinker\TinkerServiceProvider::class,
         Prettus\Repository\Providers\RepositoryServiceProvider::class,
 
         /*
