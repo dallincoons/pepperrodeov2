1bd650b733bfa1629ce3dce84f6dad09842630bb
Dallin Coons <dallincoons@Dallins-MacBook-Pro.local>
2023-01-17 20:59:35 -0600

    remove spark service provider

--- config/app.php
+++ config/app.php
@@ -171,12 +171,9 @@ return [
         /*
          * Application Service Providers...
          */
-        Laravel\Spark\Providers\SparkServiceProvider::class,
-        App\Providers\SparkServiceProvider::class,
         Laravel\Cashier\CashierServiceProvider::class,
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         App\Providers\RepositoryServiceProvider::class
